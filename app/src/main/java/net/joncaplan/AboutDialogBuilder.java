e46fb8677d16f7014da47cca765f592f1e3b51bd
joncaplan <joncaplan@gmail.com>
2014-07-13 21:30:05 -0400

    Added PayPal Donate button to about box and added large version of the SimplyWeather logo to the about box.

--- app/src/main/java/net/joncaplan/AboutDialogBuilder.java
+++ app/src/main/java/net/joncaplan/AboutDialogBuilder.java
@@ -5,9 +5,6 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.text.SpannableString;
-import android.text.util.Linkify;
-import android.widget.TextView;
 import android.webkit.WebView;
 
 // Code borrowed from: http://www.itkrauts.com/archives/26-Creating-a-simple-About-Dialog-in-Android-1.6.html
@@ -31,15 +28,59 @@ public class AboutDialogBuilder {
         else
             aboutText = "Simply Weather gives you just the forecast, in detail. " +
                     "Swipe left or right to see table of weather data. <br><br>" +
-                    "Feedback and bug reports welcome at:<br>simplyweather@joncaplan.net <br>(c) 2014 Jon Caplan. <br><br>" +
+                    "Feedback and bug reports welcome at:<br>simplyweather@joncaplan.net<br><br>" +
                     "This program is open source and available under the GPL 2 license at GitHub.  " +
-                    "<a href='http://github.com/joncaplan/SimplyWeather'>github.com/joncaplan/SimplyWeather</a><br><br>" +
+                    "<a href='http://github.com/joncaplan/SimplyWeather'>github.com/joncaplan/SimplyWeather</a>" +
+                    "<br>(c) 2014 Jon Caplan.<br><br>" +
                     "About, add, delete and refresh icons from: " +
                     "<a href='http://www.visualpharm.com'>www.visualpharm.com</a> under CC license.<br><br>"+
-                    "Version " + versionString ;
+                    "<img src='http://ecx.images-amazon.com/images/I/71S-R%2BOZ1dL._SL500_AA300_.png' >"
+                    ;
+
+        /*String donateHeader = "<script type=\"text/javascript\">" +
+                "/* <![CDATA[ * /" +
+                "(function() {" +
+                "    var s = document.createElement('script');" +
+                "    var t = document.getElementsByTagName('script')[0];" +
+                "    s.type = 'text/javascript';" +
+                "    s.async = true;" +
+                "    s.src = '//api.flattr.com/js/0.6/load.js?" +
+                "mode=auto&" +
+                "uid=joncaplan&" +
+                "language=en_US&" +
+                "category=software&" +
+                "mode=auto';" +
+                "    t.parentNode.insertBefore(s, t);" +
+                " })();" +
+                "/* ]]> *n" +
+                "</script>";
+
+        String donateButton_Flattr =
+                "<br>" +
+                "    <a class='FlattrButton' " +
+                "    title='This is my title'" +
+                "    data-flattr-uid='joncaplan'" +
+                "    data-flattr-category='software'" +
+                "    href='http://apps.joncaplan.net/'>" +
+                "    Button Text." +
+                "</a>";
+        */
+
+        String donateButton_PayPal = "I've made this program free and open source, but " +
+                "if you'd like to contribute a buck or two to say thanks, just use the PayPal button below.<br><br>"+
+                "<center>" +
+                "<form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">\n" +
+                        "<input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\n" +
+                        "<input type=\"hidden\" name=\"hosted_button_id\" value=\"ZP6F22F5ANF5G\">\n" +
+                        "<input type=\"image\" src=\"https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal - The safer, easier way to pay online!\">\n" +
+                        "<img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_US/i/scr/pixel.gif\" width=\"1\" height=\"1\">\n" +
+                        "</form>" +
+                        "</center>";
 
         final WebView message = new WebView(context);
-        String aboutBoxHTML = "<HTML><BODY>" + aboutText + "</BODY></HTML>";
+        String aboutBoxHTML = "<HTML><HEAD></HEAD><BODY>" +
+                aboutText + donateButton_PayPal + "<br>" + versionString +
+                "</BODY></HTML>";
         message.loadData(aboutBoxHTML, "text/html", "utf-8");
         return new AlertDialog.Builder(context).setTitle(aboutTitle).setCancelable(true).setIcon(R.drawable.icon).setPositiveButton(
                 context.getString(android.R.string.ok), null).setView(message).create();
